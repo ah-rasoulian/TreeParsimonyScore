0493193e77cb5eb634e036e7aa9f4f44f0fd9bcb
Amirhossein <ah.rasoulian@gmail.com>
2021-06-13 21:22:46 +0430

    add a function which calculates small parsimony per position.

--- main.py
+++ main.py
@@ -63,6 +63,61 @@ def create_tree(node_ids, edges, leaves, number_of_chars):
         tree.get(parent_id).add_edge(child_id)
 
 
+def small_parsimony(char_position):
+    global tree
+    for node in tree.values():
+        node: Node
+        node.tag = 0
+        if node.sequence is not None:  # if node is leaf
+            node.tag = 1
+            for char in CHARS:
+                if node.sequence[char_position] == char:
+                    node.S.get(char_position)[char] = 0
+    ripe_nodes_ids = get_ripe_nodes()
+    while len(ripe_nodes_ids) > 0:
+        node = tree.get(ripe_nodes_ids[0])
+        node.tag = 1
+        for char in CHARS:
+            left_min = math.inf
+            daughter_node = tree.get(node.left_child_id)
+            daughter_node: Node
+            for char2 in CHARS:
+                possible_cost = daughter_node.S.get(char_position).get(char2) + mutation_cost(char, char2)
+                if possible_cost < left_min:
+                    left_min = possible_cost
+
+            right_min = math.inf
+            son_node = tree.get(node.right_child_id)
+            son_node: Node
+            for char2 in CHARS:
+                possible_cost = son_node.S.get(char_position).get(char2) + mutation_cost(char, char2)
+                if possible_cost < right_min:
+                    right_min = possible_cost
+
+            node.S.get(char_position)[char] = left_min + right_min
+        ripe_nodes_ids = get_ripe_nodes()
+
+    root_node = tree.get(0)
+    root_node: Node
+    minimum_parsimony_in_position = min(root_node.S.get(char_position).values())
+    return minimum_parsimony_in_position
+
+
+def get_ripe_nodes():
+    global tree
+    ripe_nodes = []
+    for node_id, node in tree.items():
+        node: Node
+        if node.tag == 0 and tree.get(node.left_child_id).tag == 1 and tree.get(node.right_child_id).tag == 1:
+            ripe_nodes.append(node_id)
+    return ripe_nodes
+
+def mutation_cost(char1, char2):
+    if char1 == char2:
+        return 0
+    else:
+        return 1
+
 tree = {}
 CHARS = ['A', 'C', 'G', 'T']
 
