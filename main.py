ee44a37e47e441706cb41be033f5a9f04b65a566
Amirhossein <ah.rasoulian@gmail.com>
2021-06-13 19:36:56 +0430

    reading input.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    pass
+    number_of_nodes = int(input())
+    edges = []
+    for i in range(number_of_nodes - 1):
+        edge = input().split(" ")
+        edges.append([int(edge[0]), int(edge[1])])
+
+    number_of_leaves = int(input())
+    leaves = {}
+    for i in range(number_of_leaves):
+        leaf = input().split(" ")
+        leaves[int(leaf[0])] = leaf[1]
 
 
 if __name__ == '__main__':
