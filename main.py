82775aeebdebaa843b24df0d993240fbad4fa1f5
Amirhossein <ah.rasoulian@gmail.com>
2021-06-13 20:13:33 +0430

    a variable is added to each node which stores its parsimony score for a specific character.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import math
 
 
 class Node:
-    def __init__(self, sequence=None):
+    def __init__(self, number_of_chars, sequence=None):
         self.sequence = sequence
 
         self.left_child_id = None
         self.right_child_id = None
         self.tag = 0
-        self.S = {}
+        self.S = {}  # for each char position, for each char, its value is parsimony score until current node for that character
+        for i in range(number_of_chars):
+            self.S[i] = {'A': math.inf, 'C': math.inf, 'G': math.inf, 'T': math.inf}
 
     def add_edge(self, child_id):
         if self.left_child_id is None:
@@ -30,24 +32,28 @@ def main():
 
     number_of_leaves = int(input())
     leaves = {}
+    number_of_chars = 0
+
     for i in range(number_of_leaves):
         leaf = input().split(" ")
-        leaves[int(leaf[0])] = leaf[1]
+        if i == 0:
+            number_of_chars = len(leaf[1])
+        leaves[int(leaf[0])] = leaf[1].upper()
 
-    create_tree(node_ids, edges, leaves)
+    create_tree(node_ids, edges, leaves, number_of_chars)
 
 
-def create_tree(node_ids, edges, leaves):
+def create_tree(node_ids, edges, leaves, number_of_chars):
     global tree
     # creating leaf nodes
     for leaf_id in leaves.keys():
-        new_node = Node(sequence=leaves.get(leaf_id))
+        new_node = Node(number_of_chars=number_of_chars, sequence=leaves.get(leaf_id))
         node_ids.remove(leaf_id)
         tree[leaf_id] = new_node
 
     # creating other nodes
     for node_id in node_ids:
-        new_node = Node()
+        new_node = Node(number_of_chars=number_of_chars)
         tree[node_id] = new_node
 
     # adding edges
@@ -58,5 +64,7 @@ def create_tree(node_ids, edges, leaves):
 
 
 tree = {}
+CHARS = ['A', 'C', 'G', 'T']
+
 if __name__ == '__main__':
     main()
